ebefba8076005dae5625e51f0c08a706b4527ec6
lkx <1102185964@qq.com>
2017-03-06 04:32:49 +0000

    “init”

---
+++ src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span class='title'>
+      欢迎登陆
+    </span>
+    <divider>Vue Vux</divider>
+    <Group>
+      <XInput title='账号' v-model='account' placeholder='账号' type='text'></XInput>
+    </Group>
+    <x-input type="password" title='密码' v-model='password' placeholder='密码'/>
+    <XButton type='primary' action-type='button'>登陆</XButton>
+  </div>
+</template>
+<script>
+  import { Divider, Group, XInput, XButton } from 'vux'
+  export default {
+    components: {
+      Divider,
+      Group,
+      XInput,
+      XButton
+    },
+    data () {
+      return {
+        account: '',
+        password: ''
+      }
+    }
+  }
+</script>
